0d1acf5ad92c5b7e00be3ea33769ae0c53e6e2a0
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-27 01:44:36 +0600

    adding & fetchingcountry

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const url = "https://restcountries.eu/rest/v2/all"
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setCountries(data))
+  }, [])
+
   return (
     <div className="App">
+      <h1>CountryLoaded:{countries.length}</h1>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
